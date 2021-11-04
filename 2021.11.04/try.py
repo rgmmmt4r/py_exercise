d583363d54902efc85d3391a844367c70e66a912
Eddie Yen <b04303117@ntu.edu.tw>
2021-11-04 23:20:43 +0800

    11.04.04

--- 2021.11.04/try.py
+++ 2021.11.04/try.py
@@ -1,12 +1,15 @@
 #輸入數字，每個數字之間都有一個間隔，並且轉成list[int]
-input_string = input()
-nums_list = input_string.split()
-for i in range(len(nums_list)):
-    nums_list[i] = int(nums_list[i])
+while(True):
+    input_string = input("請輸入數字，每個數字之間都有一個間隔隔開:")
+    nums_list = input_string.split()
+    try:
+        for i in range(len(nums_list)):
+            nums_list[i] = int(nums_list[i])
+        break
+    except ValueError:
+        print("您輸入的不是整數，請重新輸入")
 
-
-
-target = int(input())
+target = int(input("請輸入target:"))
 answer = []
 
 #用雙層for迴圈窮舉找出答案
@@ -16,7 +19,5 @@ for i in range(0,len(nums_list)-1):
         #print("j: " + str(j) + " ")
         if nums_list[i] + nums_list[j] == target:
             answer = [i , j]
-
-
 
 print(answer)

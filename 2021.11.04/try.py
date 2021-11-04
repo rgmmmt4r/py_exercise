f4357046f5ef80040c1cf153835123960d032c07
Eddie Yen <b04303117@ntu.edu.tw>
2021-11-04 14:58:29 +0800

    110402

--- 2021.11.04/try.py
+++ 2021.11.04/try.py
@@ -1,18 +1,23 @@
-#題目：https://leetcode.com/problems/two-sum/
-#輸入array 並且轉成list[int]
+#輸入數字，每個數字之間都有一個間隔，並且轉成list[int]
 input_string = input()
 nums_list = input_string.split()
 for i in range(len(nums_list)):
     nums_list[i] = int(nums_list[i])
 
+
+
 target = int(input())
 answer = []
 
 #用雙層for迴圈窮舉找出答案
-for i in range(len(nums_list)):
-    for j in range(1,len(nums_list)-1):
+for i in range(0,len(nums_list)-1):
+    #print("i: " + str(i)+ " ")
+    for j in range(i+1,len(nums_list)):
+        #print("j: " + str(j) + " ")
         if nums_list[i] + nums_list[j] == target:
             answer = [i , j]
+            break
         else:
             j = j + 1
+            continue
 print(answer)

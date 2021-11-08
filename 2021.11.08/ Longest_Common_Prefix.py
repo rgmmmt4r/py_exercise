73359fce167a447f94ff9e5967ed7158cb0385f2
Eddie Yen <b04303117@ntu.edu.tw>
2021-11-08 22:42:13 +0800

    110802

--- 2021.11.08/ Longest_Common_Prefix.py
+++ 2021.11.08/ Longest_Common_Prefix.py
@@ -9,7 +9,7 @@ class Solution:
                 return ""
             else:
               return strs[0]
-        elif n == 0:
+        elif n == 0:     
             return ""
         d = {}         # d 用於存每個 str[0] 中的每個 prefix，key 即是str[0]的每個prefix，value 即是該prefix出現次數
         max_value = 0  # 用於存出現最多次的 prefix，其出現的次數
@@ -18,28 +18,20 @@ class Solution:
         #先將str[0] 中的每個prefix 存入 d
         for j in range(1,len(strs[0])+1):
             d[strs[0][0:j]] = 1
-
+        #在計算每個str[i]，共同出現的最長prefix
         for i in range(1,n):
-            if strs[i] == "":
+            if strs[i] == "":   #如果其中有個str[i] 是 "" ，即可確認沒有common prefix
                     return ""
             for j in range(1,len(strs[i])+1):
-                if strs[i][0:j] in d:
-                    d[strs[i][0:j]] =  d[strs[i][0:j]] + 1
-                    if d[strs[i][0:j]]  >=  max_value:
+                if strs[i][0:j] in d: #如果確認同樣prefix 出現在 str[i][0:j]，d[strs[i][0:j]] 即加一
+                    d[strs[i][0:j]] =  d[strs[i][0:j]] + 1 
+                    if d[strs[i][0:j]]  >=  max_value: #比較出最多次的 common prefix
                         max_key = strs[i][0:j]
                         max_value = d[strs[i][0:j]]
-                '''
-                else:
-                    if j-1 >= 1 :
-                        if strs[i][0:j-1] in d:
-                            del d[strs[i][0:j-1]]
-                    else:
-                        return ""
-                
-                '''
+
         if max_key != "":
             for i in range(1,n):
-                if strs[i][0] != max_key[0]:
+                if strs[i][0] != max_key[0]: #處理input 像是["aa","aa","b"] 的情況
                         return ""
         return max_key
 

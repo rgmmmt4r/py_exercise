56e4e02c9ff2e1f261cbe84692c54d22dfe87469
Eddie Yen <36756732+rgmmmt4r@users.noreply.github.com>
2023-01-26 17:41:00 +0800

    100.04

---
+++ leetcode/easy/100/100.04.py
@@ -0,0 +1,16 @@
+class Solution:
+    result = True
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        
+        def inorder_at_the_same_time(p,q):
+            if p and q:
+                inorder_at_the_same_time(p.left,q.left)
+                if p.val != q.val:
+                    self.result = False
+                inorder_at_the_same_time(p.right,q.right)
+            elif p and not q:
+                self.result = False
+            elif not  p and q:
+                self.result = False
+        inorder_at_the_same_time(p,q)
+        return self.result
